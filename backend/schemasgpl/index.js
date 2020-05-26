2fdc61f35ae8d54998d9cb5bd7c84f4a9fc95fa1
longdt <longdt@elephas.vn>
2020-05-26 13:41:31 +0700

    Add relation betwwen product & category

--- backend/schemasgpl/index.js
+++ backend/schemasgpl/index.js
@@ -1,7 +1,38 @@
-const { schemaComposer } = require('graphql-compose');
+const {
+    schemaComposer
+} = require('graphql-compose');
 
 // Put schemas here ...
-const product = require('./product');
+const productTC = require('./product');
+const productCategoryTC = require('./product_category');
+
+
+productCategoryTC.addRelation(
+    'products', {
+        resolver: () => productTC.getResolver('findMany'),
+        prepareArgs: {
+            filter: source => ({
+                category_id: source._id
+            }),
+        },
+        projection: {
+            _id: true
+        },
+    }
+);
+
+
+productTC.addRelation('category_id', {
+    resolver: () => productCategoryTC.getResolver('findById'),
+    prepareArgs: {
+        _id: source => source._id,
+        skip: null,
+        sort: null,
+    },
+    projection: {
+        _id: true
+    },
+})
 
 // Build schemas
 const graphqlSchema = schemaComposer.buildSchema();
